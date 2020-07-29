d5570f5e58158f91ac755c86ac54bf63428b1c3d
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-07-28 18:17:21 -0700

    setup files
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- hacks/setup-krustlet.sh
+++ hacks/setup-krustlet.sh
@@ -1,8 +1,14 @@
+#!/usr/bin/env bash
+rm -rf $HOME/.krustlet
+/usr/local/bin/kubectl delete certificatesigningrequests.certificates.k8s.io krustletwascc krustletwascc-tls
+
 set -e
+BASEDIR=$(dirname "$0")
 
 kubectl cluster-info --context kind-kind
-bash bootstrap-krustlet.sh
+bash $BASEDIR/bootstrap-krustlet.sh
 export KUBECONFIG=$HOME/.krustlet/config/kubeconfig
-hostname="krustletwascc"
-echo Run  "kubectl certificate approve $hostname" in a different terminal
-krustlet-wascc --node-ip 10.0.0.115 --cert-file=$HOME/.krustlet/config/krustlet.crt --private-key-file=$HOME/.krustlet/config/krustlet.key --bootstrap-file=/Users/ramiro/.krustlet/config/bootstrap.conf --hostname $hostname
+echo -e Run  "\033[0;31mkubectl certificate approve krustletwascc-tls\033[0m" in a different terminal
+#/Users/ramiro/code/krustlet/target/debug/krustlet-wascc --node-ip 10.0.0.115 --cert-file=$HOME/.krustlet/config/krustlet.crt --private-key-file=$HOME/.krustlet/config/krustlet.key --bootstrap-file=/Users/ramiro/.krustlet/config/bootstrap.conf --hostname krustletwascc
+krustlet-wascc --node-ip 10.0.0.115 --cert-file=$HOME/.krustlet/config/krustlet.crt --private-key-file=$HOME/.krustlet/config/krustlet.key --bootstrap-file=/Users/ramiro/.krustlet/config/bootstrap.conf --hostname krustletwascc
+
--- hacks/setup-openfaas.sh
+++ hacks/setup-openfaas.sh
@@ -1,6 +1,6 @@
-kind create cluster
+kubectl cluster-info --context kind-kind
 arkade install openfaas --clusterrole --basic-auth=false --operator --pull-policy Always --wait
 kubectl apply -f profile-wasi.yaml
 kubectl apply -f profile-wascc.yaml
-echo "port-forwarding the gateway over http://localhost:8080"
-kubectl port-forward -n openfaas svc/gateway 8080:8080
+echo "port-forwarding the gateway over http://localhost:8000"
+kubectl port-forward -n openfaas svc/gateway 8000:8080
